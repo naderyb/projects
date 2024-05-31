5e73886b8f5b7f733b500d477eb38a8299f33337
NebulaNexux <163655137+naderyb@users.noreply.github.com>
2024-05-31 22:20:12 +0100

    Add files via upload

---
+++ waitEnter.c
@@ -0,0 +1,18 @@
+/*youb nader 
+instgram: unnamed0._
+class: 1y */
+
+#include <stdio.h>
+
+//function to wait for user to press enter to continue
+void attendreEntree() {
+    printf("Press enter to continue...");
+    getchar(); 
+}
+
+int main(){
+    printf("hello\n ");
+    attendreEntree();
+    printf("computer science student!!!!");
+    return 0;
+}
